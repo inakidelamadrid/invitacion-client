51c09a920ef702d8ce4801bbe90ae16683baee70
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-11-26 17:15:36 -0600

    Display invitee list in a table

--- src/components/display/InviteeList/index.js
+++ src/components/display/InviteeList/index.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import styles from './styles.module.scss'
 
+const Invitee = ({ invitee }) => {
+  const inviteState = {
+    notsent: 'not sent yet',
+    pending: 'pending',
+  }
 
-const Invitee = ({ name }) => {
-  return <div className="">{name}</div>
+  return (
+    <tr>
+      <td>{invitee.name}</td>
+      <td>{invitee.lastName}</td>
+      <td>{invitee.facebookPath}</td>
+      <td>Invite {inviteState.pending}| copy invite</td>
+      <td>Generate invite link </td>
+    </tr>
+  )
 }
 
-const InviteeList = ({invitees }) => {
+const InviteeList = ({ invitees }) => {
   return (
     <div className="inviteesListWrapper">
       <h1> Invitados </h1>
-      {invitees.map((invitee, index) => (
-        <Invitee key={index} name={invitee.name} />
-      ))}
+      <table className={styles.inviteeTable}>
+        <thead>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Facebook</th>
+          <th>Estado de invitacion</th>
+          <th>Link de invitacion</th>
+        </thead>
+        <tbody>
+          {invitees.map((invitee, index) => (
+            <Invitee key={index} invitee={invitee} />
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/reducers/invitees.js
+++ src/reducers/invitees.js
@@ -1,4 +1,8 @@
-const initial = [{ name: 'Teresa' }, { name: 'Mara' }, { name: 'Loren' }]
+const initial = [
+  { name: 'Teresa', lastName: 'Cruz', facebookPath: 'tere.cruz' },
+  { name: 'Mara', lastName: 'De La Madrid', facebookPath: 'loren.cernas' },
+  { name: 'Loren', lastName: 'Cernas', facebookPath: 'fabiola.delamadrid' },
+]
 
 const inviteesReducer = (state = initial, action) => {
   switch (action.type) {
