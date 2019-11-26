60abb7221123f11a2681e84b730eae4238c4f6ab
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-11-26 14:08:15 -0600

    Add the InviteeList to the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router'
+import logo from './logo.svg'
+import './App.css'
+
+import InviteeList from './components/page/InviteeList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="bg-blue-600">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p className="bg-blue-600">
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </Route>
+        <Route path="/invitees">
+          <InviteeList />
+        </Route>
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/page/InviteeList/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const InviteeList = ({ data }) => {
+  return <div>InviteeList</div>
+}
+export default InviteeList
