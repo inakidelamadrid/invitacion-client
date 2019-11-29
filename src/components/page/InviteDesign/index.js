d0b9ae51b63467db68d0443a3597d7f28707e52f
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-11-29 14:12:19 -0600

    Invite is partial. Needs internal design

--- src/components/page/InviteDesign/index.js
+++ src/components/page/InviteDesign/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import styles from './styles.module.scss'
+//https://www.greetingsisland.com/preview/invitations/comic-dots/314-10868
 
-const InviteDesign = props => {
-  return <div>InviteDesign</div>
+const InviteDesign = ({name="Anonimo"}) => {
+  return (
+    <div className={styles.inviteWrapper}>
+      <div className={styles.invite}>
+      </div>
+      <div className={styles.inviteDetails}>
+        <p>Superheroe {name}, estas invitad@!</p>
+        <p>Nos reuniremos en Capitan Llerenas #97, Comala</p>
+        <p>Domingo | Enero 5 | 2020 </p>
+      </div>
+    </div>
+  )
 }
 export default InviteDesign
