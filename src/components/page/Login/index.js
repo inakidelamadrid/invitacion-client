0e4ec1eda89a41a64969a916da335bf812ca1bee
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-12-10 14:10:21 -0600

    Add and style login page

--- src/components/page/Login/index.js
+++ src/components/page/Login/index.js
@@ -1,19 +1,24 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './styles.module.scss'
+
 //import logoImg from "../img/logo.jpg";
 //import { Card, Logo, Form, Input, Button } from '../components/AuthForms';
 
 function Login() {
   return (
-    <div className='card'>
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button>Sign In</button>
+    <div className={styles.card}>
+      <form className={styles.form}>
+        <input className={styles.input} type="email" placeholder="email" />
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="password"
+        />
+        <button className={styles.button}>Sign In</button>
       </form>
-      <Link to="/signup">Don't have an account?</Link>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
