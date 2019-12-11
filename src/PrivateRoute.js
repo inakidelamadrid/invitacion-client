3f72b1e03561cba39c55028070469e2f983d641f
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-12-11 13:17:45 -0600

    Login page and PrivateRoute:

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -7,7 +7,9 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={props => (logged ? <Component {...props} /> : <Redirect to="" />)}
+      render={props =>
+        logged ? <Component {...props} /> : <Redirect to="/login" />
+      }
     />
   )
 }
--- src/components/page/Login/index.js
+++ src/components/page/Login/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styles from './styles.module.scss'
 
-//import logoImg from "../img/logo.jpg";
-//import { Card, Logo, Form, Input, Button } from '../components/AuthForms';
 
 function Login() {
   return (
