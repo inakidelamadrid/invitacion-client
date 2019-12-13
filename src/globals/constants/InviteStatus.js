620471bca01e71ba69695ab51ff3e0318b3c8470
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2019-12-13 11:29:23 -0600

    Add an option to see all invites

--- src/globals/constants/InviteStatus.js
+++ src/globals/constants/InviteStatus.js
@@ -9,7 +9,7 @@ export const statuses = [ACCEPTED, MAYBE, NOTSENT, PENDING, REJECTED]
 export const inviteStatusMap = {
   accepted: 'Si iré',
   maybe: 'Tal vez iré',
-  notsent: 'Invitacion no enviada',
+  notsent: 'Todas',
   pending: 'Invitación aún no respondida',
   rejected: 'No puedo ir',
 }
